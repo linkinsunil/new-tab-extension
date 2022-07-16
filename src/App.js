3eb6d2797ca54e9ad4b07cc83628ec1544c7fc0a
Sunil Vishwakarma <sunilnet4@gmail.com>
2022-07-16 23:31:14 +0530

    time and date card completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Time from './Components/Time/Time';
 
 function App() {
-  return <div className='App'>React</div>;
+  return (
+    <div className='App'>
+      <Time />
+    </div>
+  );
 }
 
 export default App;
