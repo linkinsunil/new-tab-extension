5c9fe1b7b622242a8da954c5c89e38c05b355cdd
Sunil Vishwakarma <sunilnet4@gmail.com>
2022-07-20 20:30:14 +0530

    fix: sompleted app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import './App.css';
+import Bookmarks from './Components/Bookmarks/Bookmarks';
 import Todo from './Components/Todo/Todo';
-// import Crypto from './Components/Crypto/Crypto';
-// import Weather from './Components/Weather/Weather';
-// import Greeting from './Components/Greeting/Greeting';
-// import Time from './Components/Time/Time';
+import Crypto from './Components/Crypto/Crypto';
+import Weather from './Components/Weather/Weather';
+import Greeting from './Components/Greeting/Greeting';
+import Time from './Components/Time/Time';
 
 function App() {
   return (
-    <div className='App'>
-      {/* <Time />
-      <Greeting />
-      <Weather />
-      <Crypto /> */}
-      <Todo />
+    <div className='app-wrapper'>
+      <div className='App'>
+        <div className='item1'>
+          <Bookmarks />
+        </div>
+        <div className='item2'>
+          <Time />
+        </div>
+        <div className='item3'>
+          <Greeting />
+        </div>
+        <div className='item4'>
+          <Weather />
+        </div>
+        <div className='item5'>
+          <Todo />
+        </div>
+        <div className='item6'>
+          <Crypto />
+        </div>
+      </div>
     </div>
   );
 }
